3b30f0284c610fe7b98b25cdd677e3dd6f1c8d5a
Donald Wu <yeukfei02@gmail.com>
2020-08-23 14:21:49 +0800

    refactor: 💡 fix test case

--- src/test/main.test.tsx
+++ src/test/main.test.tsx
@@ -3,9 +3,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, mount } from 'enzyme';
 import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
-import axios from 'axios';
-import { TMDB_API_KEY } from 'react-native-dotenv';
-import { getRootUrl } from '../common/Common';
 
 import MainView from '../components/mainView/MainView';
 
@@ -20,9 +17,6 @@ import Info from '../components/info/Info';
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
-
-const ROOT_URL = getRootUrl();
-let movieId = '';
 
 describe('main.test', () => {
   describe('render test', () => {
@@ -54,106 +48,6 @@ describe('main.test', () => {
     it('Info', () => {
       const wrapper = shallow(<Info />);
       expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-  describe('api request test', () => {
-    it('search movies request test', async () => {
-      const response = await axios.get(`${ROOT_URL}/search/movie`, {
-        params: {
-          api_key: TMDB_API_KEY,
-          query: 'alad',
-          language: 'en-US',
-          page: 1,
-        },
-      });
-      const responseData = response.data;
-      // console.log('responseData = ', responseData);
-
-      expect(responseData).toBeDefined();
-      expect(responseData.results).toBeDefined();
-
-      movieId = responseData.results[0].id;
-
-      if (responseData) {
-        responseData.results.forEach((item: any, i: number) => {
-          expect(item.id).toBeDefined();
-          expect(typeof item.id).toBe('number');
-
-          expect(item.title).toBeDefined();
-          expect(typeof item.title).toBe('string');
-
-          expect(item.poster_path).toBeDefined();
-          // expect(typeof item.poster_path).toBe('string');
-
-          expect(item.vote_average).toBeDefined();
-          expect(typeof item.vote_average).toBe('number');
-        });
-      }
-    });
-
-    it('movies popular request test', async () => {
-      const response = await axios.get(`${ROOT_URL}/movie/popular`, {
-        params: {
-          api_key: TMDB_API_KEY,
-          language: 'en-US',
-          page: 1,
-        },
-      });
-      const responseData = response.data;
-      // console.log('responseData = ', responseData);
-
-      expect(responseData).toBeDefined();
-      expect(responseData.results).toBeDefined();
-
-      if (responseData) {
-        responseData.results.forEach((item: any, i: number) => {
-          expect(item.id).toBeDefined();
-          expect(typeof item.id).toBe('number');
-
-          expect(item.title).toBeDefined();
-          expect(typeof item.title).toBe('string');
-
-          expect(item.backdrop_path).toBeDefined();
-          // expect(typeof item.backdrop_path).toBe('string');
-        });
-      }
-    });
-
-    it('movies details request test', async () => {
-      const response = await axios.get(`${ROOT_URL}/movie/${movieId}`, {
-        params: {
-          api_key: TMDB_API_KEY,
-          language: 'en-US',
-        },
-      });
-      const responseData = response.data;
-      // console.log('responseData = ', responseData);
-
-      expect(responseData).toBeDefined();
-
-      if (responseData) {
-        expect(responseData.poster_path).toBeDefined();
-        expect(typeof responseData.poster_path).toBe('string');
-
-        expect(responseData.title).toBeDefined();
-        expect(typeof responseData.title).toBe('string');
-
-        expect(responseData.overview).toBeDefined();
-        expect(typeof responseData.overview).toBe('string');
-
-        expect(responseData.release_date).toBeDefined();
-        expect(typeof responseData.release_date).toBe('string');
-
-        expect(responseData.vote_average).toBeDefined();
-        expect(typeof responseData.vote_average).toBe('number');
-
-        expect(responseData.vote_count).toBeDefined();
-        expect(typeof responseData.vote_count).toBe('number');
-
-        expect(responseData.popularity).toBeDefined();
-        expect(typeof responseData.popularity).toBe('number');
-      }
     });
   });
 });
